4d0729cfbff7537281d79317e00490c74339cc08
Vokerlee <70313579+Vokerlee@users.noreply.github.com>
2021-04-02 15:20:04 +0300

    Update hash_functions.h

--- 5.1.  Hash-table's research/hash_functions.h
+++ 5.1.  Hash-table's research/hash_functions.h
@@ -198,12 +198,14 @@ class JenkinsHash
             uint32_t i = 0;
             uint32_t len = strlen(string);
             uint32_t hash = 0;
+            
             while (i != len)
             {
                 hash += string[i++];
                 hash += hash << 10;
                 hash ^= hash >> 6;
             }
+            
             hash += hash << 3;
             hash ^= hash >> 11;
             hash += hash << 15;
