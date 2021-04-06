1da4cdf9cfb8cf4c1abc32c5ca22a0ee42939809
Roman Glaz <70313579+Vokerlee@users.noreply.github.com>
2021-04-07 01:39:48 +0300

    Add files via upload

---
+++ 6. Alpha blending/Alpha blending.cpp
@@ -0,0 +1,12 @@
+﻿#include "alpha_blending.h"
+
+int main()
+{
+    auto back_file  = BMPFile("back.bmp");
+    auto cat_file = BMPFile("picture.bmp");
+
+    back_file.compose_alpha(cat_file, 635, 200);
+    back_file.save_to_file("composed.bmp");
+
+    return 0;
+}
